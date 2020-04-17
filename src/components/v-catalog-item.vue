3ab65f0c35e1307162cdc0cde6efaa3e11ef3f00
mihail5lub <mihlubimov@gmail.com>
2020-04-17 15:08:31 +0500

    Создание первых компонентов/подключение db.json

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="v-catalog-item">
+        <p>Item</p>
+        <p>Price: 100</p>
+        <button>Add to cart</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "v-catalog-item",
+    props: {},
+    data() {
+        return{}
+    },
+    computed: {}
+}
+</script>
+
+<style lang ="scss">
+    .v-catalog-item {
+        flex-basis: 25%;
+        box-shadow: 0 0 8px 0 #e0e0e0;
+        padding: $padding*2;
+        margin-bottom: $margin*2;
+    }
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+        <v-catalog />
+        <v-cart />
+    </div>
+</template>
+
+<script>
+    import vCatalog from './v-catalog'
+    import vCart from './v-cart'
+
+    export default {
+        name: 'v-main-wrapper',
+        components: {
+           vCatalog,
+           vCart
+        },
+        props: {
+
+        },
+        data() {
+            return {
+                title: 'Main wrapper'
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        watch: {},
+        mounted() {
+            console.log('Hello world')
+        }
+    }
+</script>
+
+<style>
+    .v-main-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto; 
+    }
+</style>
